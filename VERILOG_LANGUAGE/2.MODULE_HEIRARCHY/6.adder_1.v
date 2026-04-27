2531906381d3358f1eb0664f1a5f5e9fb17f1a54
22ece45rangarajan <22ece45rangarajan@gmail.com>
2026-04-27 19:56:03 +0530

    Create 6.adder_1.v

---
+++ VERILOG_LANGUAGE/2.MODULE_HEIRARCHY/6.adder_1.v
@@ -0,0 +1,18 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire [15:0] p,r,k,m,n,s;
+    wire cin,cout,c1;
+    assign p= a[31:16];
+    assign n= a[15:0];
+    assign k= b[31:16];
+    assign m= b[15:0];
+    assign r= sum[31:16];
+    assign s=sum[15:0];
+    assign cin=1'b0;
+    add16 add1(n,m,cin,s,c1);
+    add16 add2(p,k,c1,r,cout);
+    assign sum = {r,s};
+endmodule
